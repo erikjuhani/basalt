d25cd190feb8bdd224984ecf9144a8121c5e22fb
ikken <pierosaucedo3@gmail.com>
2025-05-08 13:08:01 -0300

    Update keybinds

--- basalt/src/events.rs
+++ basalt/src/events.rs
@@ -12,25 +12,27 @@ pub fn key_event_to_action(key_event: KeyEvent, _ctx: &Context) -> Option<Action
     match key_event.code {
         KeyCode::Char('q') => Some(Action::Quit),
         KeyCode::Char('?') => Some(Action::ToggleHelp),
-        KeyCode::Char('o') if key_event.modifiers.contains(KeyModifiers::CONTROL) => {
-            Some(Action::ToggleVaultSelector)
+        KeyCode::Char(' ') => Some(Action::ToggleVaultSelector),
+        KeyCode::Char('c') if key_event.modifiers.contains(KeyModifiers::CONTROL) => {
+            Some(Action::Quit)
         }
-        KeyCode::Char('k') | KeyCode::Up => Some(Action::Prev), // Context will determine if it's scroll or item prev
-        KeyCode::Char('j') | KeyCode::Down => Some(Action::Next), // Context will determine if it's scroll or item next
-        KeyCode::Enter => Some(Action::Select),
-        KeyCode::Tab => Some(Action::ToggleMode),
-        // KeyCode::Esc => {
-        //     // Escape could close modals or unfocus elements
-        //     // This logic is better handled inside the `update` function based on context
-        // }
-        KeyCode::PageUp => Some(Action::ScrollUp(ScrollAmount::HalfPage)),
-        KeyCode::PageDown => Some(Action::ScrollDown(ScrollAmount::HalfPage)),
         KeyCode::Char('u') if key_event.modifiers.contains(KeyModifiers::CONTROL) => {
             Some(Action::ScrollUp(ScrollAmount::HalfPage))
         }
         KeyCode::Char('d') if key_event.modifiers.contains(KeyModifiers::CONTROL) => {
             Some(Action::ScrollDown(ScrollAmount::HalfPage))
         }
+        KeyCode::PageUp => Some(Action::ScrollUp(ScrollAmount::HalfPage)),
+        KeyCode::PageDown => Some(Action::ScrollDown(ScrollAmount::HalfPage)),
+        KeyCode::Char('k') | KeyCode::Up => Some(Action::Prev), // Context will determine if it's scroll or item prev
+        KeyCode::Char('j') | KeyCode::Down => Some(Action::Next), // Context will determine if it's scroll or item next
+        KeyCode::Char('t') | KeyCode::Tab => Some(Action::ToggleMode),
+        KeyCode::Enter => Some(Action::Select),
+
+        // KeyCode::Esc => {
+        //     // Escape could close modals or unfocus elements
+        //     // This logic is better handled inside the `update` function based on context
+        // }
         _ => None,
     }
 }
